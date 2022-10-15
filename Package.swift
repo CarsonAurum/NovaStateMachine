fe084a481c74db426348210c72ce0eb2f3096491
Carson R <53197646+CarsonAurum@users.noreply.github.com>
2022-10-15 13:48:49 -0500

    updates

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -13,11 +12,7 @@ let package = Package(
         .package(name: "NovaCore", path: "../NovaCore/")
     ],
     targets: [
-        .target(
-            name: "NovaStateMachine",
-            dependencies: ["NovaCore"],
-            path: "Sources/"
-        ),
+        .target(name: "NovaStateMachine", dependencies: ["NovaCore"], path: "Sources/"),
         .testTarget(
             name: "NovaStateMachineTests",
             dependencies: ["NovaStateMachine"],
